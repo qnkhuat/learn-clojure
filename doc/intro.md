9d10fd61772861836d7cf75bc3fa2c97b8c41fff
Earther <qn.khuat@gmail.com>
2021-05-31 22:02:55 +0700

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to god-langugage
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/god_langugage/core_test.clj
@@ -0,0 +1,7 @@
+(ns god-langugage.core-test
+  (:require [clojure.test :refer :all]
+            [god-langugage.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
